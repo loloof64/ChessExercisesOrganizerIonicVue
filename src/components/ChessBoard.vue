1e35ef575088374c0f3b79793ad80d0195a544ca
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-10-22 16:29:37 +0200

    Adjusted board size
    Adjusted board to screen size, centering it, and adapted size to screen orientation.

---
+++ src/components/ChessBoard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="root" :style="{ width: sizePixels, height: sizePixels}"></div>
+</template>
+
+<script>
+import {computed} from 'vue';
+export default {
+    props: {
+        sizePx: {
+            type: Number,
+            default: 60,
+        }
+    },
+    setup(props) {
+        const sizePixels = computed(function() {
+            const {sizePx} = props;
+            return sizePx+'px';
+        });
+        return {sizePixels};
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    #root {
+        background-color: orangered;
+    }
+</style>
--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -2,27 +2,74 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Game</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Game</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+
+      <div class="board_zone" :style="{ width: sizePx, height: sizePx }">
+        <ChessBoard :sizePx="size" />
+      </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import { ref, computed } from "vue";
+import { ScreenOrientation } from "@ionic-native/screen-orientation";
+import ChessBoard from "@/components/ChessBoard.vue";
 
-export default  {
-  name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+export default {
+  name: "Game",
+  components: {
+    ChessBoard,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
+  setup() {
+    function computeSize() {
+      const orientationType = ScreenOrientation.type;
+      const isPortrait = orientationType.includes("portrait");
+      const minSize = Math.min(window.innerWidth, window.innerHeight);
+      const sizeRatio = isPortrait ? 0.9 : 0.6;
+      return Math.floor(sizeRatio * minSize);
+    }
+
+    const size = ref(computeSize());
+
+    const sizePx = computed(function () {
+      return size.value + "px";
+    });
+
+    const orientationListenerHandle = window.addEventListener(
+      "orientationchange",
+      function () {
+        size.value = computeSize();
+      }
+    );
+
+    return { size, sizePx };
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.board_zone {
+  margin: 0.9em auto;
+}
+</style>
