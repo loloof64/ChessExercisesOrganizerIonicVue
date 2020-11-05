46a2ddbdce29f27df73b5f9eb91157603a07e3cd
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-11-05 14:47:30 +0100

    Refactoring for the chessboard component #2.

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -29,9 +29,10 @@
 
 <script>
 import { createGesture } from "@ionic/vue";
-import { reactive, onMounted } from "vue";
+import { onMounted } from "vue";
 import useChessBoardLogic from "@/hooks/ChessBoardLogic";
 import useChessBoardGraphic from "@/hooks/ChessBoardGraphic";
+import useChessBoardDragAndDrop from "@/hooks/ChessBoardDragAndDrop";
 import ChessBoardMainLayer from "@/components/ChessBoardMainLayer";
 import ChessBoardDndLayer from "@/components/ChessBoardDndLayer";
 
@@ -57,104 +58,37 @@ export default {
 
     const {
       isEmptyCell,
-      isWhiteTurn,
       piecesValues,
       getRank,
       getFile,
+      isWhiteTurn,
     } = useChessBoardLogic();
 
-    const dndState = reactive({
-      started: false,
-      startFile: null,
-      startRank: null,
-      endFile: null,
-      endRank: null,
-      draggedPieceX: null,
-      draggedPieceY: null,
-      draggedPieceSrc: null,
-    });
-
-    function resetDndState() {
-      dndState.started = false;
-      dndState.startFile = null;
-      dndState.startRank = null;
-      dndState.endFile = null;
-      dndState.endRank = null;
-      dndState.draggedPieceX = null;
-      dndState.draggedPieceY = null;
-      dndState.draggedPieceSrc = null;
-    }
-
-    function handleDragStart(detail) {
-      const rootEl = document.querySelector(".board_root");
-      const x = detail.currentX - rootEl.offsetLeft;
-      const y = detail.currentY - rootEl.offsetTop;
-
-      const cellsSize = props.sizePx / 9.0;
-      const col = Math.floor((x - cellsSize * 0.5) / cellsSize);
-      const row = Math.floor((y - cellsSize * 0.5) / cellsSize);
-
-      const file = props.reversed ? 7 - col : col;
-      const rank = props.reversed ? row : 7 - row;
-      if (file < 0 || file > 7 || rank < 0 || rank > 7) return;
-      const cellValue = piecesValues.raws[rank][file];
-
-      if ([undefined, null].includes(cellValue)) return;
-      const isWhitePiece = ["P", "N", "B", "R", "Q", "K"].includes(cellValue);
-      const isBlackPiece = ["p", "n", "b", "r", "q", "k"].includes(cellValue);
-      const whiteInTurn = isWhiteTurn();
-      const isPlayerInTurnPiece =
-        (whiteInTurn && isWhitePiece) || (!whiteInTurn && isBlackPiece);
-
-      if (!isPlayerInTurnPiece) return;
-
-      dndState.started = true;
-
-      dndState.startFile = file;
-      dndState.startRank = rank;
-
-      dndState.draggedPieceX = x - cellsSize * 0.5;
-      dndState.draggedPieceY = y - cellsSize * 0.5;
-
-      dndState.draggedPieceSrc = piecesValues.paths[rank][file];
-    }
-
-    function handleDragEnd() {
-      resetDndState();
-    }
-
-    function handleDragMove(detail) {
-      if (!dndState.started) return;
-
-      const rootEl = document.querySelector(".board_root");
-      const x = detail.currentX - rootEl.offsetLeft;
-      const y = detail.currentY - rootEl.offsetTop;
-
-      const cellsSize = props.sizePx / 9.0;
-      const col = Math.floor((x - cellsSize * 0.5) / cellsSize);
-      const row = Math.floor((y - cellsSize * 0.5) / cellsSize);
-
-      const file = props.reversed ? 7 - col : col;
-      const rank = props.reversed ? row : 7 - row;
-
-      dndState.endFile = file;
-      dndState.endRank = rank;
-
-      dndState.draggedPieceX = x - cellsSize * 0.5;
-      dndState.draggedPieceY = y - cellsSize * 0.5;
-    }
-
-    function handleDragLeave() {
-      if (!dndState.started) return;
-      resetDndState();
-    }
+    const {
+      dndState,
+      handleDragStart,
+      handleDragMove,
+      handleDragEnd,
+    } = useChessBoardDragAndDrop();
 
     onMounted(function () {
       const boardGesture = createGesture({
         el: document.querySelector(".board_dnd_layer"),
-        onStart: handleDragStart,
+        onStart: (detail) =>
+          handleDragStart({
+            detail,
+            boardSizePx: props.sizePx,
+            reversed: props.reversed,
+            piecesValues,
+            whiteTurn: isWhiteTurn(),
+          }),
         onEnd: handleDragEnd,
-        onMove: handleDragMove,
+        onMove: (detail) =>
+          handleDragMove({
+            detail,
+            boardSizePx: props.sizePx,
+            reversed: props.reversed,
+          }),
         threshold: 0,
       });
 
@@ -172,7 +106,6 @@ export default {
       handleDragStart,
       handleDragEnd,
       handleDragMove,
-      handleDragLeave,
       dndState,
     };
   },
---
+++ src/hooks/ChessBoardDragAndDrop.js
@@ -0,0 +1,91 @@
+import { reactive } from "vue";
+
+export default function useChessBoardDragAndDrop() {
+  const dndState = reactive({
+    started: false,
+    startFile: null,
+    startRank: null,
+    endFile: null,
+    endRank: null,
+    draggedPieceX: null,
+    draggedPieceY: null,
+    draggedPieceSrc: null,
+  });
+
+  function resetDndState() {
+    dndState.started = false;
+    dndState.startFile = null;
+    dndState.startRank = null;
+    dndState.endFile = null;
+    dndState.endRank = null;
+    dndState.draggedPieceX = null;
+    dndState.draggedPieceY = null;
+    dndState.draggedPieceSrc = null;
+  }
+
+  function handleDragStart({detail, boardSizePx, reversed, piecesValues, whiteTurn}) {
+    const rootEl = document.querySelector(".board_root");
+    const x = detail.currentX - rootEl.offsetLeft;
+    const y = detail.currentY - rootEl.offsetTop;
+
+    const cellsSize = boardSizePx / 9.0;
+    const col = Math.floor((x - cellsSize * 0.5) / cellsSize);
+    const row = Math.floor((y - cellsSize * 0.5) / cellsSize);
+
+    const file = reversed ? 7 - col : col;
+    const rank = reversed ? row : 7 - row;
+    if (file < 0 || file > 7 || rank < 0 || rank > 7) return;
+    const cellValue = piecesValues.raws[rank][file];
+
+    if ([undefined, null].includes(cellValue)) return;
+    const isWhitePiece = ["P", "N", "B", "R", "Q", "K"].includes(cellValue);
+    const isBlackPiece = ["p", "n", "b", "r", "q", "k"].includes(cellValue);
+    const isPlayerInTurnPiece =
+      (whiteTurn && isWhitePiece) || (!whiteTurn && isBlackPiece);
+
+    if (!isPlayerInTurnPiece) return;
+
+    dndState.started = true;
+
+    dndState.startFile = file;
+    dndState.startRank = rank;
+
+    dndState.draggedPieceX = x - cellsSize * 0.5;
+    dndState.draggedPieceY = y - cellsSize * 0.5;
+
+    dndState.draggedPieceSrc = piecesValues.paths[rank][file];
+  }
+
+  function handleDragEnd() {
+    resetDndState();
+  }
+
+  function handleDragMove({detail, boardSizePx, reversed}) {
+    if (!dndState.started) return;
+
+    const rootEl = document.querySelector(".board_root");
+    const x = detail.currentX - rootEl.offsetLeft;
+    const y = detail.currentY - rootEl.offsetTop;
+
+    const cellsSize = boardSizePx / 9.0;
+    const col = Math.floor((x - cellsSize * 0.5) / cellsSize);
+    const row = Math.floor((y - cellsSize * 0.5) / cellsSize);
+
+    const file = reversed ? 7 - col : col;
+    const rank = reversed ? row : 7 - row;
+
+    dndState.endFile = file;
+    dndState.endRank = rank;
+
+    dndState.draggedPieceX = x - cellsSize * 0.5;
+    dndState.draggedPieceY = y - cellsSize * 0.5;
+  }
+
+  return {
+    dndState,
+    resetDndState,
+    handleDragStart,
+    handleDragEnd,
+    handleDragMove,
+  };
+}
