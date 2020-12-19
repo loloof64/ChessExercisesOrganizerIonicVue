119d410711b08ad814e52c9aef21a73b4783ca59
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-12-19 01:02:42 +0100

    Replaced tab 1 in main page.
    In the main page, replaced the icon and text of the tab 1.

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -3,15 +3,15 @@
     <ion-tabs>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="gameController" />
-          <ion-label>Game</ion-label>
+          <ion-icon :icon="library" />
+          <ion-label>{{getTranslation("main_page.samples_tab")}}</ion-label>
         </ion-tab-button>
-          
+
         <ion-tab-button tab="tab2" href="/tabs/tab2">
           <ion-icon :icon="ellipse" />
           <ion-label>Tab 2</ion-label>
         </ion-tab-button>
-        
+
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon :icon="square" />
           <ion-label>Tab 3</ion-label>
@@ -21,19 +21,40 @@
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
-import { ellipse, square, gameController } from 'ionicons/icons';
+<script>
+import { ref } from "vue";
+import {
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonLabel,
+  IonIcon,
+  IonPage,
+} from "@ionic/vue";
+import { ellipse, square, library } from "ionicons/icons";
+import { useI18n } from "vue-i18n";
 
 export default {
-  name: 'Tabs',
+  name: "Tabs",
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage },
   setup() {
-    return {
-      ellipse, 
-      square, 
-      gameController,
+    const locale = ref(null);
+    const { t } = useI18n();
+
+    function getTranslation(key) {
+      return t(key, {}, { locale: locale.value });
     }
-  }
-}
+
+    if (window.Intl && typeof window.Intl === "object") {
+      locale.value = navigator.language.substring(0, 2);
+    }
+
+    return {
+      ellipse,
+      square,
+      library,
+      getTranslation,
+    };
+  },
+};
 </script>
