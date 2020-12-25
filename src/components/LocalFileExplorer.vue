395929796eacfd1fb89b84363d530e9205dbc071
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-12-25 12:11:56 +0100

    We can see current path in local file explorer

--- src/components/LocalFileExplorer.vue
+++ src/components/LocalFileExplorer.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="root"></div>
+  <div class="root">
+    <div class="path">{{currentPathString}}</div>
+  </div>
 </template>
 
 <script>
-import { onMounted } from "vue";
+import { onMounted, computed } from "vue";
 import { FilesystemDirectory, Plugins } from "@capacitor/core";
 const { Filesystem } = Plugins;
+import useTranslationUtils from "@/hooks/TranslationUtils";
+
 export default {
   props: {
     path: {
@@ -14,6 +18,9 @@ export default {
     },
   },
   setup(props) {
+    const { getTranslation, initTranslationsUtils } = useTranslationUtils();
+    initTranslationsUtils();
+
     onMounted(async () => {
       try {
         await Filesystem.mkdir({
@@ -26,7 +33,13 @@ export default {
       }
     });
 
-    return {};
+    const currentPathString = computed(() => {
+      return `${getTranslation("local_explorer.path_prefix")}`;
+    });
+
+    return {
+      currentPathString,
+    };
   },
 };
 </script>
@@ -36,5 +49,13 @@ export default {
   width: 100%;
   height: 100%;
   background-color: white;
+}
+
+.path {
+  width: 100%;
+  font-size: 1.1em;
+  font-family: serif;
+  background-color: khaki;
+  overflow: scroll;
 }
 </style>
